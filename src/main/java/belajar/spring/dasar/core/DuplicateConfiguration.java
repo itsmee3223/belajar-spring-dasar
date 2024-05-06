b9138dcfdb2fbe0520507dcd27202f08bda76a01
itsmee3223 <tamvam.pemberani@gmail.com>
2024-05-06 13:07:10 +0700

    duplikat bean konsep

---
+++ src/main/java/belajar/spring/dasar/core/DuplicateConfiguration.java
@@ -0,0 +1,18 @@
+package belajar.spring.dasar.core;
+
+import belajar.spring.dasar.core.data.Foo;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DuplicateConfiguration {
+    @Bean
+    public Foo foo1(){
+        return  new Foo();
+    }
+
+    @Bean
+    public Foo foo2(){
+        return  new Foo();
+    }
+}
