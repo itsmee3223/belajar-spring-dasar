7cbbee3f9c1241e6cc6ef77587f362f7939b6aed
itsmee3223 <tamvam.pemberani@gmail.com>
2024-05-07 20:43:04 +0700

    membuat scope

--- src/main/java/belajar/spring/dasar/core/ScopeConfiguration.java
+++ src/main/java/belajar/spring/dasar/core/ScopeConfiguration.java
@@ -1,7 +1,10 @@
 package belajar.spring.dasar.core;
 
+import belajar.spring.dasar.core.data.Bar;
 import belajar.spring.dasar.core.data.Foo;
+import belajar.spring.dasar.core.scope.DoubletonScope;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.config.CustomScopeConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
@@ -18,5 +21,21 @@ public class ScopeConfiguration {
     public Foo foo(){
         log.info("create new Foo");
         return new Foo();
+    }
+
+//    gunakan kelas CustomScopeConfigurer untuk menambahkan custom scope yang sudah kitaa buat
+    @Bean
+    public CustomScopeConfigurer customScopeConfigurer(){
+        CustomScopeConfigurer configurer = new CustomScopeConfigurer();
+//        tambhakn scope yang dibuat
+        configurer.addScope("doubleton", new DoubletonScope());
+        return configurer;
+    }
+
+    @Bean
+    @Scope("doubleton")
+    public Bar bar(){
+        log.info("create new Bar");
+        return new Bar();
     }
 }
