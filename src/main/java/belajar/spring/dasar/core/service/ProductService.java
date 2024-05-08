23103245164b92f0d607359264367054241bd193
itsmee3223 <tamvam.pemberani@gmail.com>
2024-05-08 21:20:24 +0700

    notasi component untuk membuat bean

---
+++ src/main/java/belajar/spring/dasar/core/service/ProductService.java
@@ -0,0 +1,7 @@
+package belajar.spring.dasar.core.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProductService {
+}
---
+++ src/test/java/belajar/spring/dasar/core/ComponentTest.java
@@ -0,0 +1,21 @@
+package belajar.spring.dasar.core;
+
+import belajar.spring.dasar.core.service.ProductService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ComponentTest {
+    private ConfigurableApplicationContext applicationContext;
+    @BeforeEach
+    void setUp(){
+        applicationContext = new AnnotationConfigApplicationContext(ComponentConfiguration.class);
+        applicationContext.registerShutdownHook();
+    }
+
+    @Test
+    void testService(){
+        ProductService productService = applicationContext.getBean(ProductService.class);
+    }
+}
