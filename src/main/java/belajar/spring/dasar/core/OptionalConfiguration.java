b56c9103ce210077965060e37bf1b08efc14844c
itsmee3223 <tamvam.pemberani@gmail.com>
2024-05-08 22:13:44 +0700

    optional parameter

---
+++ src/main/java/belajar/spring/dasar/core/OptionalConfiguration.java
@@ -0,0 +1,22 @@
+package belajar.spring.dasar.core;
+
+import belajar.spring.dasar.core.data.Bar;
+import belajar.spring.dasar.core.data.Foo;
+import belajar.spring.dasar.core.data.FooBar;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Optional;
+
+@Configuration
+public class OptionalConfiguration {
+    @Bean
+    public Foo foo(){
+        return new Foo();
+    }
+
+    @Bean
+    public FooBar fooBar(Optional<Foo> foo, Optional<Bar> bar){
+        return new FooBar(foo.orElse(null), bar.orElse(null));
+    }
+}
